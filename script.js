52255ef4f3112ad0103bfd8dbeb7086ecb9289a4
Victor Saprykin <victanik@mail.ru>
2024-06-27 15:07:34 +0300

    Add slider

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
